f5c34b9981796f188c8200a6a27de9a2dd8d5b58
Anthony Ettinger <anthony@chovy.com>
2013-08-15 02:51:41 -0700

    get inbox list. and sent list.

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,74 @@
-var log = function(m){
-    console && console.log(m);
-};
+(function($){
+    var bm;
 
-$(function(){
-    log('ready');
-});
+    var ui = {
+        error: function(m){
+            $("#msg").addClass('err').html(m).fadeIn();
+        },
+
+        ok: function(m){
+            $("#msg").addClass('ok').html(m).fadeIn();
+            ui.hideMsg();
+        },
+
+        hideMsg: function(){
+            setTimeout(function(){
+                $("#msg").hide().removeClass('ok err').html('');
+            }, 5000);
+        }
+    };
+
+    $(function(){
+        log('bmr ready...');
+
+        $("#login").on('submit', login);
+    });
+
+    function login(e){
+        e.preventDefault();
+
+        var $form = $(e.target)
+            , user = $form.find('input[name=user]').val()
+            , pass = $form.find('input[name=pass]').val()
+            , host = $form.find('input[name=host]').val()
+            , port = $form.find('input[name=port]').val()
+            ;
+
+        bm = require('bitmessage-node')(host, port, user, pass);
+
+        ui.ok('Successfully connected to API');
+
+        $("#login").fadeOut(function(){
+            getInbox();
+            $("#inbox").fadeIn();
+        });
+
+    }
+
+    function getInbox(){
+        try {
+            bm.messages.inbox.list(function(msgs) {
+                console.dir(msgs);
+            });
+        } catch (err) {
+            console.dir(err);
+        }
+    }
+
+    function sentMessages(){
+        try {
+            bm.messages.sent.list(function(msgs) {
+                console.dir(msgs);
+            });
+        } catch (err){
+            console.dir(err);
+        }
+    }
+
+    function log(m){
+        console && console.log(m);
+    }
+})(jQuery);
+
+
+
