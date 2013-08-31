1f36c24ceca5d9338b48108c6ab40f011aa83259
Anthony Ettinger <anthony@chovy.com>
2013-08-31 14:29:33 -0700

    login defaults.

--- app/js/templates.js
+++ app/js/templates.js
@@ -6,6 +6,6 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   
 
 
-  return "<form id=\"login\" class=\"clearfix\" method=\"post\">\n    <fieldset>\n        <p><label for=\"user\">Username</label> <input type=\"text\" name=\"user\" id=\"user\" value=\"payton\"></p>\n        <p><label for=\"password\">Password</label> <input type=\"password\" id=\"password\" name=\"pass\" value=\"manning\"></p>\n        <p><label for=\"host\">Host</label> <input type=\"text\" id=\"host\" name=\"host\" value=\"bitmessagehost\"></p>\n        <p><label for=\"port\">Port</label> <input type=\"text\" id=\"port\" name=\"port\" value=\"9442\"></p>\n    </fieldset>\n    <button type=\"submit\">Login</button>\n</form>";
+  return "<form id=\"login\" class=\"clearfix\" method=\"post\">\n    <fieldset>\n        <p><label for=\"user\">Username</label> <input type=\"text\" name=\"user\" id=\"user\" value=\"payton\"></p>\n        <p><label for=\"password\">Password</label> <input type=\"password\" id=\"password\" name=\"pass\" value=\"manning\"></p>\n        <p><label for=\"host\">Host</label> <input type=\"text\" id=\"host\" name=\"host\" value=\"bitmessagehost\"></p>\n        <p><label for=\"port\">Port</label> <input type=\"text\" id=\"port\" name=\"port\" value=\"8442\"></p>\n    </fieldset>\n    <button type=\"submit\">Login</button>\n</form>";
   });
 })();
--- app/templates/login.tpl
+++ app/templates/login.tpl
@@ -3,7 +3,7 @@
         <p><label for="user">Username</label> <input type="text" name="user" id="user" value="payton"></p>
         <p><label for="password">Password</label> <input type="password" id="password" name="pass" value="manning"></p>
         <p><label for="host">Host</label> <input type="text" id="host" name="host" value="bitmessagehost"></p>
-        <p><label for="port">Port</label> <input type="text" id="port" name="port" value="9442"></p>
+        <p><label for="port">Port</label> <input type="text" id="port" name="port" value="8442"></p>
     </fieldset>
     <button type="submit">Login</button>
 </form>
