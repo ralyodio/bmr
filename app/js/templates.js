19f310949b9b8f90ffffdfc6edda1c36e00dc639
Anthony Ettinger <anthony@chovy.com>
2013-09-19 14:27:26 -0700

    gitignore

--- app/js/templates.js
+++ app/js/templates.js
@@ -313,7 +313,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   
 
 
-  return "<form id=\"login\" class=\"clearfix\" method=\"post\">\n    <fieldset>\n        <p><label for=\"user\">Username</label> <input type=\"text\" name=\"user\" id=\"user\" value=\"\"></p>\n        <p><label for=\"password\">Password</label> <input type=\"password\" id=\"password\" name=\"pass\" value=\"\"></p>\n        <p><label for=\"host\">Host</label> <input type=\"text\" id=\"host\" name=\"host\" value=\"\"></p>\n        <p><label for=\"port\">Port</label> <input type=\"text\" id=\"port\" name=\"port\" value=\"8442\"></p>\n        <p class=\"remember\"><input type=\"checkbox\" id=\"remember\" name=\"remember\" value=\"1\"> <label for=\"remember\">Remember me</label></p>\n    </fieldset>\n    <button type=\"submit\">Login</button>\n</form>";
+  return "<form id=\"login\" class=\"clearfix\" method=\"post\">\n    <fieldset>\n        <p><label for=\"user\">Username</label> <input type=\"text\" name=\"user\" id=\"user\" value=\"\"></p>\n        <p><label for=\"password\">Password</label> <input type=\"password\" id=\"password\" name=\"pass\" value=\"\"></p>\n        <p><label for=\"host\">Host</label> <input type=\"text\" id=\"host\" name=\"host\" value=\"localhost\"></p>\n        <p><label for=\"port\">Port</label> <input type=\"text\" id=\"port\" name=\"port\" value=\"8442\"></p>\n        <p class=\"remember\"><input type=\"checkbox\" id=\"remember\" name=\"remember\" value=\"1\"> <label for=\"remember\">Remember me</label></p>\n    </fieldset>\n    <button type=\"submit\">Login</button>\n</form>";
   });
 templates['message'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
--- app/templates/login.tpl
+++ app/templates/login.tpl
@@ -2,7 +2,7 @@
     <fieldset>
         <p><label for="user">Username</label> <input type="text" name="user" id="user" value=""></p>
         <p><label for="password">Password</label> <input type="password" id="password" name="pass" value=""></p>
-        <p><label for="host">Host</label> <input type="text" id="host" name="host" value=""></p>
+        <p><label for="host">Host</label> <input type="text" id="host" name="host" value="localhost"></p>
         <p><label for="port">Port</label> <input type="text" id="port" name="port" value="8442"></p>
         <p class="remember"><input type="checkbox" id="remember" name="remember" value="1"> <label for="remember">Remember me</label></p>
     </fieldset>
